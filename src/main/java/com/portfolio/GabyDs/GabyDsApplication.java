db0fbac1d77ecc946aa364aad2b7c5affdcc20aa
GabyDs <gabydsch2001@gmail.com>
2023-01-07 00:07:16 -0300

    firt commit with spring boot project

---
+++ src/main/java/com/portfolio/GabyDs/GabyDsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.GabyDs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabyDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabyDsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/GabyDs/GabyDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.GabyDs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabyDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
