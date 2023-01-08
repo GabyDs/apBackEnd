57113a60b4f24589621ee12d084876fa6de922cc
GabyDs <gabydsch2001@gmail.com>
2023-01-08 16:10:13 -0300

    add find profile

--- src/main/java/com/portfolio/GabyDs/Controller/PersonaController.java
+++ src/main/java/com/portfolio/GabyDs/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.GabyDs.Entity.Persona;
 import com.portfolio.GabyDs.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+// error CORS policy
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -50,6 +53,11 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
     
 }
